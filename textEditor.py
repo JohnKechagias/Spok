f2b7040031cc5af40dbe66dfc9bfe3bd5f95d1f8
John Kechagias <kechagiasjohn@yahoo.gr>
2022-04-01 00:39:17 +0300

    created a seperate class for NumberedText

--- textEditor.py
+++ textEditor.py
@@ -1,5 +1,6 @@
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
+import tkinter as tk
 
 class TextEditor(ttk.Frame):
 
@@ -16,17 +17,14 @@ class TextEditor(ttk.Frame):
         super().__init__(master, padding=padding)
 
         self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
         self.columnconfigure(1, weight=1)
-        self.columnconfigure(2, weight=1)
 
         # setup text widget
-        self._numberedText = ttk.Text(master=self, width=3)
-        self._numberedText.tag_configure('line', justify=RIGHT)
-        self._text = cText(master=self, wrap=WORD, **kwargs)
+        self._numberedText = NumberedText(master=self)
+        self._text = CText(master=self, wrap=WORD, **kwargs)
         self._hbar = None
         self._vbar = None
-
-        self._numOfTextLines = 0
 
         self._numberedText.grid(row=0, column=0, sticky=NS)
         self._text.grid(row=0, column=1, sticky=NSEW)
@@ -46,7 +44,7 @@ class TextEditor(ttk.Frame):
                 command=self._scrollBoth,
                 orient=VERTICAL,
             )
-            self._vbar.grid(row=0, column=2, sticky=NSEW)
+            self._vbar.grid(row=0, column=2, sticky=NS)
             self._numberedText.configure(yscrollcommand=self._updateScroll)
             self._text.configure(yscrollcommand=self._updateScroll)
 
@@ -57,7 +55,7 @@ class TextEditor(ttk.Frame):
                 command=self._text.xview,
                 orient=HORIZONTAL,
             )
-            self._hbar.grid(row=1, column=1, columnspan=3, sticky=NSEW)
+            self._hbar.grid(row=1, column=0, columnspan=3, sticky=EW)
             self._text.configure(xscrollcommand=self._hbar.set)
 
         # position scrollbars
@@ -73,13 +71,8 @@ class TextEditor(ttk.Frame):
             self.hide_scrollbars()
 
     def _onChange(self, *_):
-        numberOfTextLines = self._text.count('1.0', END, 'displaylines')[0]
-
-        if numberOfTextLines != self._numOfTextLines:
-            self._numOfTextLines = numberOfTextLines
-            self._numberedText.delete('1.0', END)
-            for i in range(numberOfTextLines):
-                self._numberedText.insert(f'{i + 1}.0', f'{i + 1}\n')
+        newNumOfLines = self._text.count('1.0', END, 'displaylines')[0]
+        self._numberedText.setNumOfLines(newNumOfLines)
 
     
     def _scrollBoth(self, action, position, type=None):
@@ -96,7 +89,30 @@ class TextEditor(ttk.Frame):
             self._numberedText.yview(*args)
 
 
-class cText(ttk.Text):
+class CText(ttk.Text):
+    """Text widget which generates an event '<<TextChanged>>' whenever
+    the text is modified or the view is changed.
+
+    STANDARD OPTIONS
+
+        background, borderwidth, cursor,
+        exportselection, font, foreground,
+        highlightbackground, highlightcolor,
+        highlightthickness, insertbackground,
+        insertborderwidth, insertofftime,
+        insertontime, insertwidth, padx, pady,
+        relief, selectbackground,
+        selectborderwidth, selectforeground,
+        setgrid, takefocus,
+        xscrollcommand, yscrollcommand,
+
+    WIDGET-SPECIFIC OPTIONS
+
+        autoseparators, height, maxundo,
+        spacing1, spacing2, spacing3,
+        state, tabs, undo, width, wrap,
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -133,3 +149,47 @@ class cText(ttk.Text):
 
         # return what the actual widget returned
         return result
+        
+
+class NumberedText(ttk.Text):
+    """Text widget where each line is a number. Numbers are
+    sorted in ascending order.
+    
+    STANDARD OPTIONS
+
+        background, borderwidth, cursor,
+        exportselection, font, foreground,
+        highlightbackground, highlightcolor,
+        highlightthickness, insertbackground,
+        insertborderwidth, insertofftime,
+        insertontime, insertwidth, padx, pady,
+        relief, selectbackground,
+        selectborderwidth, selectforeground,
+        setgrid, takefocus,
+        xscrollcommand, yscrollcommand,
+
+    WIDGET-SPECIFIC OPTIONS
+
+        autoseparators, height, maxundo,
+        spacing1, spacing2, spacing3,
+        state, tabs, undo, width, wrap,
+    """
+    
+    def __init__(self, width=3, *args, **kwargs):
+        super().__init__(width=width, border=0, *args, **kwargs)
+        # move every index to the right of the line
+        self.tag_configure('tag_right', justify=RIGHT)
+        self.tag_configure('tag_selected', background='white')
+        self._numOfLines = 0
+
+    def setNumOfLines(self, newNumOfLines):
+        if self._numOfLines != newNumOfLines:
+            self._numOfLines = newNumOfLines
+            self._recalculateNumbers()
+
+    def _recalculateNumbers(self):
+        # clear all indexes
+        self.delete('1.0', END)
+        # recalculate all line indexes
+        for i in range(self._numOfLines):
+            self.insert(f'{i + 1}.0', f'{i + 1}\n', 'tag_right')
