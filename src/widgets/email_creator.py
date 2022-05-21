cc8e53166f68105aca6a7e93e65323d793951748
John Kechagias <kechagiasjohn@yahoo.gr>
2022-05-21 23:53:51 +0300

    Refactor email_creator

--- src/widgets/email_creator.py
+++ src/widgets/email_creator.py
@@ -18,41 +18,41 @@ class EmailCreator(ttk.Frame):
         # Disables batch sending and enables the sending
         # of personal emails (with a single recipient).
         self.personal_email = ttk.BooleanVar(value=False)
-        self.personal_email.trace_add('write', self.personal_email_changed)
+        self.personal_email.trace_add('write', self._on_personal_email_bool_changed)
         self.email_signature = ''
 
         self._subject = PlaceholderEntry(self, placeholder='Subject')
         self._subject.grid(row=1,  column=0, sticky=EW, pady=(0, 5))
 
-        self._receiver = PlaceholderEntry(self, placeholder='To')
+        self._to = PlaceholderEntry(self, placeholder='To')
 
         self._body = ttk.Text(self, *args, **kwargs)
         self._body.grid(row=2,  column=0, sticky=NSEW, pady=(5, 0))
 
         self._body.bind('<KeyPress-t>', lambda _: self.get_email())
 
-    def personal_email_changed(self, *args):
+    def _on_personal_email_bool_changed(self, *args):
         if self.personal_email.get():
-            self._receiver.grid(row=0,  column=0, sticky=EW, pady=(5,10))
+            self._to.grid(row=0,  column=0, sticky=EW, pady=(5,10))
         else:
-            self._receiver.grid_remove()
+            self._to.grid_remove()
 
     def get_email(self) -> Dict[str, str]:
-        """Returns the email info in a form of a dict.
-        Email info consists of the title, the recipient and the body of
-        the email in HTML form. If the personalEmail flag is false, the
-        recipient is empty.
+        """ Return the email info in a form of a dict.
+            Email info consists of the title, the recipient and the body of
+            the email in HTML form. If the personalEmail flag is false, the
+            recipient is empty.
 
-        Returns:
-            dict: email info
+            Returns:
+                dict: email info
         """
         email = {}
         email['subject'] = self._subject.get()
 
-        recipient = ''
+        to = ''
         if self.personal_email:
-            recipient = self._receiver.get()
-        email['to'] = recipient
+            to = self._to.get()
+        email['to'] = to
 
         signature = self.get_signature()
         body = self._body.get('1.0', END)
@@ -63,7 +63,7 @@ class EmailCreator(ttk.Frame):
         email['body'] = body
         return email
 
-    def get_signature(self):
+    def get_signature(self) -> str:
         with open('emailSignature.html', 'r') as file:
             signature = file.read()
         return signature
