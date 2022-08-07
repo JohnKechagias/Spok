7bce70e0988711f43144a876d138297e987c09a6
John Kechagias <kechagiasjohn@yahoo.gr>
2022-08-07 21:09:19 +0300

    Update file_dialogs comments

--- src/file_dialog.py
+++ src/file_dialog.py
@@ -7,7 +7,11 @@ import os
 
 def open_folder(path: Path) -> None:
     """ Platform independent func to open a folder in the default
-    folder dialog. """
+    folder dialog.
+
+    Args:
+        path: Folder path.
+    """
     if sys.platform == 'darwin':
         subprocess.check_call(['open', path])
     elif sys.platform == 'linux2' or sys.platform == 'linux':
